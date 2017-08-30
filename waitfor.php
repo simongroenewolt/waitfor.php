58ae07c4ebb9dcf010de5fdf3a14616628ae93d2
Simon Groenewolt <simon@ijsfontein.nl>
2017-08-30 17:36:44 +0200

    Initial version

---
+++ waitfor.php
@@ -0,0 +1,33 @@
+<?php
+
+//var_dump($argv);
+
+function stest($ip, $port) {
+    $fp = @fsockopen($ip, $port, $errno, $errstr, 0.1);
+    if (!$fp) {
+        return false;
+    } else {
+        fclose($fp);
+        return true;
+    }
+}
+
+$startTime = time();
+$timeout = 15; //timeout in seconds
+
+$ip = $argv[1];
+$port = $argv[2];
+
+while(true)
+{
+    if(time() > $startTime + $timeout) {
+        exit(1);
+    }
+
+    // check port
+    if (stest($ip, $port)) {
+        exit(0);
+    } else {
+        sleep(0.5);
+    }
+}
